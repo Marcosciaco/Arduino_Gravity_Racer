5d13a1f5d5fcd601eb0b40dcd7b8cb42e8cfc388
Sciaco01 <36106337+Sciaco01@users.noreply.github.com>
2019-04-12 11:54:22 +0200

    added another rgb fade effect

--- Seifenkiste_spoiler_lights_V1.0/Seifenkiste_spoiler_lights_V1.0.ino
+++ Seifenkiste_spoiler_lights_V1.0/Seifenkiste_spoiler_lights_V1.0.ino
@@ -3,7 +3,7 @@
 int potVal = 0;
 int angle = 0;
 static const int potPin = A0;
-==========================================RGB======================================
+//==========================================RGB======================================
 int rVal = 255;
 int gVal = 0;
 int bVal = 127;
@@ -12,22 +12,49 @@ int rDir = -1;
 int gDir = 1;
 int bDir = -1;
 
-const int rPin = 11;
+int black[3]  = { 0, 0, 0 };
+int white[3]  = { 100, 100, 100 };
+int red[3]    = { 100, 0, 0 };
+int green[3]  = { 0, 100, 0 };
+int blue[3]   = { 0, 0, 100 };
+int yellow[3] = { 40, 95, 0 };
+int dimWhite[3] = { 30, 30, 30 };
+
+int redVal = black[0];
+int grnVal = black[1]; 
+int bluVal = black[2];
+
+int wait = 10;      // 10ms internal crossFade delay; increase for slower fades
+int hold = 0;       // Optional hold when a color is complete, before the next crossFade
+int DEBUG = 1;      // DEBUG counter; if set to 1, will write values back via serial
+int loopCount = 60; // How often should DEBUG report?
+int repeat = 3;     // How many times should we loop before stopping? (0 for no stop)
+int j = 0;          // Loop counter for repeat
+
+int prevR = redVal;
+int prevG = grnVal;
+int prevB = bluVal;
+
+const int rPin = 9;
 const int gPin = 10;
-const int bPin = 9;
-==========================================SPR======================================
+const int bPin = 11;
+//==========================================SPR======================================
 Servo servo;
 
 void setup() {
  servo.attach(9);
- pinMode(,OUTPUT);
- pinMode(,OUTPUT);
- pinMode(,OUTPUT);
+ pinMode(rPin,OUTPUT);
+ pinMode(gPin,OUTPUT);
+ pinMode(bPin,OUTPUT);
 }
 
 void loop() {
   potspoiler();
   rgb();
+  crossFade(red);
+  crossFade(green);
+  crossFade(blue);
+  crossFade(yellow);
 }
 
 void potspoiler() {
@@ -59,4 +86,73 @@ void rgb(){
     bDir = bDir * -1;
   }
   delay(10);
+}
+
+int calculateStep(int prevValue, int endValue) {
+  int step = endValue - prevValue; // What's the overall gap?
+  if (step) {                      // If its non-zero, 
+    step = 1020/step;              //   divide by 1020
+  } 
+  return step;
+}
+
+int calculateVal(int step, int val, int i) {
+
+  if ((step) && i % step == 0) { // If step is non-zero and its time to change a value,
+    if (step > 0) {              //   increment the value if step is positive...
+      val += 1;           
+    } 
+    else if (step < 0) {         //   ...or decrement it if step is negative
+      val -= 1;
+    } 
+  }
+  // Defensive driving: make sure val stays in the range 0-255
+  if (val > 255) {
+    val = 255;
+  } 
+  else if (val < 0) {
+    val = 0;
+  }
+  return val;
+}
+
+void crossFade(int color[3]) {
+  // Convert to 0-255
+  int R = (color[0] * 255) / 100;
+  int G = (color[1] * 255) / 100;
+  int B = (color[2] * 255) / 100;
+
+  int stepR = calculateStep(prevR, R);
+  int stepG = calculateStep(prevG, G); 
+  int stepB = calculateStep(prevB, B);
+
+  for (int i = 0; i <= 1020; i++) {
+    redVal = calculateVal(stepR, redVal, i);
+    grnVal = calculateVal(stepG, grnVal, i);
+    bluVal = calculateVal(stepB, bluVal, i);
+
+    analogWrite(rPin, redVal);   // Write current values to LED pins
+    analogWrite(gPin, grnVal);      
+    analogWrite(bPin, bluVal); 
+
+    delay(wait); // Pause for 'wait' milliseconds before resuming the loop
+
+    if (DEBUG) { // If we want serial output, print it at the 
+      if (i == 0 or i % loopCount == 0) { // beginning, and every loopCount times
+        Serial.print("Loop/RGB: #");
+        Serial.print(i);
+        Serial.print(" | ");
+        Serial.print(redVal);
+        Serial.print(" / ");
+        Serial.print(grnVal);
+        Serial.print(" / ");  
+        Serial.println(bluVal); 
+      } 
+      DEBUG += 1;
+    }
+  }
+  prevR = redVal; 
+  prevG = grnVal; 
+  prevB = bluVal;
+  delay(hold); // Pause for optional 'wait' milliseconds before resuming the loop
 }
